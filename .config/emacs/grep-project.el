8c40142a22d9c7d3f1ae5d12814d9e4ac90b3028
Ashik80 <ashikurrahman020995@gmail.com>
2024-06-20 20:16:41 +0600

    feat: exclude files while grepping

--- .config/emacs/grep-project.el
+++ .config/emacs/grep-project.el
@@ -6,16 +6,17 @@
 
 (require 'projectile)
 (defvar exclude-dirs-grep "{node_modules,.git,__pycache__,.next,build,.build,dist,target}")
+(defvar exclude-files-grep "{yarn.lock,package-lock.json}")
 (defun my-grep-whole-project(search-term base-directory)
   "Search the whole project for SEARCH-TERM and BASE-DIRECTORY."
   (interactive "sSearch Expression: \nsBase Directory:")
   (projectile-with-default-dir (projectile-project-root)
-    (grep (concat "grep --exclude-dir=" exclude-dirs-grep " --color -Ri -nH --null -e " search-term " " base-directory))))
+    (grep (concat "grep --exclude=" exclude-files-grep " --exclude-dir=" exclude-dirs-grep " --color -Ri -nH --null -e \"" search-term "\" " base-directory))))
 (defun my-grep-whole-project-full-word(search-term base-directory)
   "Search the whole project for SEARCH-TERM and BASE-DIRECTORY."
   (interactive "sSearch Word: \nsBase Directory:")
   (projectile-with-default-dir (projectile-project-root)
-    (grep (concat "grep --exclude-dir=" exclude-dirs-grep " --color -Ri -w -nH --null -e " search-term " " base-directory))))
+    (grep (concat "grep --exclude=" exclude-files-grep " --exclude-dir=" exclude-dirs-grep " --color -Ri -w -nH --null -e \"" search-term "\" " base-directory))))
 
 ;; Grepping shortcut
 (define-key 'c-l (kbd "f g") 'my-grep-whole-project)
