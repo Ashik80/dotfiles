0c02a0cfe80ee2c85e6b72b0bb500b1f468ce7e5
Ashik80 <ashikurrahman020995@gmail.com>
2025-01-10 21:39:23 +0600

    feat: use nvui for themeing and other plugins

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,11 +1,14 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46_cache/"
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -21,3 +24,7 @@ require("lazy").setup({
   checker = { enabled = false },
   change_detection = { enabled = false },
 })
+
+for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+  dofile(vim.g.base46_cache .. v)
+end
--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -2,6 +2,7 @@ return {
   {
     'notken12/base46-colors',
     lazy = false,
+    enabled = false,
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'ayu_dark'
@@ -10,5 +11,20 @@ return {
         hi NvimTreeWinSeparator guifg=bg
       ]]
     end
-  }
+  },
+
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end
+  },
+
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
 }
