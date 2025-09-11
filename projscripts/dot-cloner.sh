bc8bad5c4d5054bdfb03e197606c65105af20c38
Ashik80 <ashikurrahman020995@gmail.com>
2025-09-11 21:22:06 +0600

    feat: add cloner scripts

---
+++ projscripts/dot-cloner.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+add_git_permissions() {
+	git config core.sshCommand "ssh -i ~/.ssh/personal"
+	git config user.name "Ashik80"
+	git config user.email "ashikurrahman020995@gmail.com"
+}
+
+if [ $(ls -A ~/Documents | wc -l) -eq 0 ]; then
+	cd ~/Documents
+	GIT_SSH_COMMAND="ssh -i ~/.ssh/personal" git clone git@github.com:Ashik80/Documents.git .
+	add_git_permissions
+fi
+if [ ! -d gpg-keys ]; then
+	cd ~
+	GIT_SSH_COMMAND="ssh -i ~/.ssh/personal" git clone git@github.com:Ashik80/gpg-keys.git
+	cd gpg-keys
+	add_git_permissions
+fi
+if [ ! -d .password-store ]; then
+	cd ~
+	GIT_SSH_COMMAND="ssh -i ~/.ssh/personal" git clone git@github.com:Ashik80/password-store.git .password-store
+	cd .password-store
+	add_git_permissions
+fi
+if [ ! -d dotfiles ]; then
+	cd ~
+	GIT_SSH_COMMAND="ssh -i ~/.ssh/personal" git clone git@github.com:Ashik80/dotfiles.git
+	cd dotfiles
+	add_git_permissions
+fi
---
+++ projscripts/manzil-project-cloner.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+DIR=$HOME/src/ManzilApp
+
+add_git_permissions() {
+	git config core.sshCommand "ssh -i ~/.ssh/office"
+	git config user.name "ashikur-rahman-sazim"
+	git config user.email "ashikur.rahman@sazim.io"
+}
+
+if [ ! -d "$DIR" ]; then
+    mkdir -p "$DIR"
+    cd $DIR
+fi
+
+if [ ! -d "$DIR"/manzil ]; then
+    cd "$DIR"
+    GIT_SSH_COMMAND="ssh -i ~/.ssh/office" git clone git@github.com:Manzilapp/manzil.git
+    cd manzil
+    add_git_permissions
+fi
+
+if [ ! -d "$DIR"/platform-be ]; then
+    cd "$DIR"
+    GIT_SSH_COMMAND="ssh -i ~/.ssh/office" git clone git@github.com:Manzilapp/platform-be.git
+    cd platform-be
+    add_git_permissions
+fi
+
+if [ ! -d "$DIR"/platform-fe ]; then
+    cd "$DIR"
+    GIT_SSH_COMMAND="ssh -i ~/.ssh/office" git clone git@github.com:Manzilapp/platform-fe.git
+    cd platform-fe
+    add_git_permissions
+fi
