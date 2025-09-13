9c58a619474bc95d1e9d9e66f7f1c61122b3beae
Ashik80 <ashikurrahman020995@gmail.com>
2025-09-14 01:19:01 +0600

    feat: add deltagram-cloner script

---
+++ projscripts/deltagram-cloner.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+add_git_permissions() {
+	git config core.sshCommand "ssh -i ~/.ssh/personal"
+	git config user.name "Ashik80"
+	git config user.email "ashikurrahman020995@gmail.com"
+}
+
+if [ ! -d $HOME/src/Deltagram ]; then
+    cd $HOME/src
+
+    GIT_SSH_COMMAND="ssh -i ~/.ssh/personal" git clone --recurse-submodules git@github.com:The-Deltagram/Deltagram.git
+
+    ln -s $HOME/Documents/Deltagram/deltagram-env $HOME/src/Deltagram/.env
+
+    cd $HOME/src/Deltagram
+    add_git_permissions
+fi
+
+if [ -d $HOME/src/Deltagram/server ] && [ ! -f $HOME/src/Deltagram/server/.env ]; then
+    ln -s $HOME/Documents/Deltagram/server-env $HOME/src/Deltagram/server/.env
+    cd $HOME/src/Deltagram/server
+    add_git_permissions
+fi
+
+if [ -d $HOME/src/Deltagram/client ] && [ ! -f $HOME/src/Deltagram/client/.env ]; then
+    ln -s $HOME/Documents/Deltagram/client-development-env $HOME/src/Deltagram/client/.env.development
+    cd $HOME/src/Deltagram/client
+    add_git_permissions
+fi
+
+if [ -d $HOME/src/Deltagram/admin ] && [ ! -f $HOME/src/Deltagram/admin/.env ]; then
+    ln -s $HOME/Documents/Deltagram/client-development $HOME/src/Deltagram/admin/.env.development
+    cd $HOME/src/Deltagram/admin
+    add_git_permissions
+fi
