7d4af2e9e1b0a8cce9c84d7bcf3459c81e67961a
Ashik80 <ashikurrahman020995@gmail.com>
2025-09-15 14:39:44 +0600

    feat: add script to inc/dec brightness

--- projscripts/setdwmbar.sh
+++ projscripts/setdwmbar.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
 # Volume
-VOLUME="󰕾 $(pactl get-sink-volume "$(pactl get-default-sink)" | grep -o '[0-9]*%' | head -n 1)"
+VOL_MUTE_STATUS="$(pactl get-sink-mute @DEFAULT_SINK@)"
+if [ "$VOL_MUTE_STATUS" = "Mute: yes" ]; then
+    VOLUME_ICON="󰝟"
+else
+    VOLUME_ICON="󰕾"
+fi
+
+MIC_MUTE_STATUS="$(pactl get-source-mute @DEFAULT_SINK@)"
+if [ "$MIC_MUTE_STATUS" = "Mute: yes" ]; then
+    MIC_ICON="󰍭"
+else
+    MIC_ICON="󰍬"
+fi
+
+VOLUME_LEVEL="$(pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]*%' | head -n 1)"
+MIC_LEVEL="$(pactl get-source-volume @DEFAULT_SOURCE@ | grep -o '[0-9]*%' | head -n 1)"
+VOLUME="${VOLUME_ICON} ${VOLUME_LEVEL} ${MIC_ICON} ${MIC_LEVEL}"
 
 # Date
-DATE=" $(date "+%e %b %Y, %I:%M %P")"
+DATE=" $(date "+%e %b %Y, %a %I:%M %P")"
 
 # Wifi
 NETWORK_NAME=$(iwctl station wlan0 show | grep 'Connected network' | sed 's/\s*Connected network\s*//' | sed 's/\s*$//')
---
+++ projscripts/xbrightness.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+BRIGHTNESS_DIR="/sys/class/backlight/intel_backlight"
+FILE="${BRIGHTNESS_DIR}/brightness"
+MAX_FILE="${BRIGHTNESS_DIR}/max_brightness"
+
+add_perm() {
+    sudo chmod 666 "$FILE"
+}
+
+if [ ! -r "$FILE" ]; then
+    add_perm
+fi
+
+if [ ! -w "$FILE" ]; then
+    add_perm
+fi
+
+MIN=10
+MAX="$(cat "$MAX_FILE")"
+CURRENT=$(cat "$FILE")
+NEW_VAL="$CURRENT"
+CMD="$1"
+PERCENT="$2"
+
+VAL=$(( MAX * PERCENT / 100 ))
+
+if [ "$CMD" = -inc ]; then
+    NEW_VAL=$(( CURRENT + VAL ))
+elif [ "$CMD" = -dec ]; then
+    NEW_VAL=$(( CURRENT - VAL ))
+elif [ "$CMD" = -set ]; then
+    NEW_VAL=$(( VAL ))
+fi
+
+echo "Prev value: $CURRENT"
+echo "New value: $NEW_VAL"
+
+if [ $NEW_VAL -gt $MAX ]; then
+    NEW_VAL=$MAX
+elif [ $NEW_VAL -lt $MIN ]; then
+    NEW_VAL=$MIN
+fi
+
+echo "$NEW_VAL" > "$FILE"
