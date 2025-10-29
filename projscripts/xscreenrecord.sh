a2edd3889652e316bbd290a735e6bca09fc47089
Ashik80 <ashikurrahman020995@gmail.com>
2025-10-29 16:42:38 +0600

    feat: make ffmpeg videos compatible with signal

--- projscripts/xscreenrecord.sh
+++ projscripts/xscreenrecord.sh
@@ -7,23 +7,29 @@ SCREEN_RES=$(xrandr --current | grep '\*' | awk '{print $1}' | head -n 1)
 start () {
     case "$1" in
         root)
-            ffmpeg -f x11grab -framerate 30 -video_size "$SCREEN_RES" -i "$DISPLAY" -c:v libx264 -preset ultrafast -crf 18 "$FILENAME" &
+            ffmpeg -f x11grab -framerate 30 -video_size "$SCREEN_RES" -i "$DISPLAY" -c:v libx264 -preset ultrafast -crf 18 -pix_fmt yuv420p "$FILENAME" &
             echo "$!" > /tmp/xscreenrecord.pid
             ;;
         root-audio)
-            ffmpeg -f x11grab -framerate 30 -video_size "$SCREEN_RES" -i "$DISPLAY" -f pulse -i default -c:v libx264 -preset ultrafast -crf 18 -c:a aac "$FILENAME" &
+            ffmpeg -f x11grab -framerate 30 -video_size "$SCREEN_RES" -i "$DISPLAY" -f pulse -i default -c:v libx264 -preset ultrafast -crf 18 -pix_fmt yuv420p -c:a aac "$FILENAME" &
             echo "$!" > /tmp/xscreenrecord.pid
             ;;
         region)
             REGION=$(slop -D -f "%X %Y %H %W") || exit 1
             read -r X Y H W <<< "$REGION"
-            ffmpeg -f x11grab -framerate 30 -video_size "${W}x${H}" -i "${DISPLAY}+${X},${Y}" -c:v libx264 -preset ultrafast -crf 18 "$FILENAME" &
+            # to make the region divisible by 2
+            (( W = W / 2 * 2 ))
+            (( H = H / 2 * 2 ))
+            ffmpeg -f x11grab -framerate 30 -video_size "${W}x${H}" -i "${DISPLAY}+${X},${Y}" -c:v libx264 -preset ultrafast -crf 18 -pix_fmt yuv420p "$FILENAME" &
             echo "$!" > /tmp/xscreenrecord.pid
             ;;
         region-audio)
             REGION=$(slop -D -f "%X %Y %H %W") || exit 1
             read -r X Y H W <<< "$REGION"
-            ffmpeg -f x11grab -framerate 30 -video_size "${W}x${H}" -i "${DISPLAY}+${X},${Y}" -f pulse -i default -c:v libx264 -preset ultrafast -crf 18 -c:a aac "$FILENAME" &
+            # to make the region divisible by 2
+            (( W = W / 2 * 2 ))
+            (( H = H / 2 * 2 ))
+            ffmpeg -f x11grab -framerate 30 -video_size "${W}x${H}" -i "${DISPLAY}+${X},${Y}" -f pulse -i default -c:v libx264 -preset ultrafast -crf 18 -pix_fmt yuv420p -c:a aac "$FILENAME" &
             echo "$!" > /tmp/xscreenrecord.pid
             ;;
         *)
