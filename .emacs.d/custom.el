ff39e386b70108b1febfd93c32810226e98aeda7
Ashik80 <ashikurrahman020995@gmail.com>
2026-07-07 00:20:43 +0600

    update gruvbox themes to add height to org headers

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("73d5813da95b1fef3c01e2b766116d6d451406f7bc6eec7de7c8b0e7bb1ce162" "179eb9bf737a16a08ff99792ac9a002f1adf012fa36cb111aad6756cb950de05" "abf44d1ffd1422e0d43687dc5dd75f5fb31127e0bab9120338e471b48ff02e52" "c93fa0319081ab2f768e2ba500f3606295833209c40f338d0e737ee47e2ae6f8" default))
+   '("8a55a300683208aeb86ec114be086105ef40b472f23c54b45ad1a22198c4eb35" "abf44d1ffd1422e0d43687dc5dd75f5fb31127e0bab9120338e471b48ff02e52" "42acdb6a6dce99606e3136078addf13f4ba9e0333907b1374ec2112dbf56818f" "247c401f693346c4e58ebeeb79ef80a41cde0c3d1c6995cf5e2e8d6441039d5b" default))
  '(package-selected-packages
    '(rainbow-mode ghostel vertico rust-mode orderless markdown-mode marginalia magit go-mode flymake-eslint exec-path-from-shell diff-hl corfu)))
 (custom-set-faces
