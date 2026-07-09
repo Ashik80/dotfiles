ac788ee9d3cc51a3a28d6202293b36326432ba20
Ashik80 <ashikurrahman020995@gmail.com>
2026-07-09 19:10:12 +0600

    fix loading themes to not mess up emacs session

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,5 +1,10 @@
+(defun my/disable-themes-before-load (theme &rest _)
+  (mapc #'disable-theme custom-enabled-themes))
+
+(advice-add 'load-theme :before #'my/disable-themes-before-load)
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;; (load-theme 'gruvbox-light-medium t)
+(load-theme 'gruvbox-light-medium t)
 ;; (load-theme 'gruvbox-dark-hard t)
-(load-theme 'kanagawa-wave t)
+;; (load-theme 'kanagawa-wave t)
 ;; (load-theme 'kanagawa-lotus t)
